93de38d36d40ed08a1d726fb9918cbc831d58a13
Andrey Tarantsov <andrey@tarantsov.com>
2023-09-29 19:10:51 +0400

    Support datetime-local  in InputTime

--- forms/controls-time.go
+++ forms/controls-time.go
@@ -12,20 +12,45 @@ type InputTime struct {
 	TagOpts
 	*Binding[time.Time]
 	InputWellOpts
-	Min      time.Time
-	Max      time.Time
-	Location *time.Location
+	IncludeTime bool
+	Min         time.Time
+	Max         time.Time
+	Location    *time.Location
 }
 
 func (InputTime) DefaultTemplate() string { return "control-datetime-date" }
 
+func (c *InputTime) InputType() string {
+	if c.IncludeTime {
+		return "datetime-local"
+	} else {
+		return "date"
+	}
+}
+
+func (c *InputTime) ValueFormat() string {
+	if c.IncludeTime {
+		return "2006-01-02T15:04"
+	} else {
+		return time.DateOnly
+	}
+}
+
+func (c *InputTime) PresentationFormat() string {
+	if c.IncludeTime {
+		return "2006-01-02 15:04"
+	} else {
+		return time.DateOnly
+	}
+}
+
 func (c *InputTime) Finalize(state *State) {
 	c.Validate(func(value time.Time) (time.Time, error) {
 		if !c.Min.IsZero() && value.Before(c.Min) {
-			return value, fmt.Errorf("must be on or after %v", c.Min)
+			return value, fmt.Errorf("must be on or after %v", c.Min.Format(c.PresentationFormat()))
 		}
 		if !c.Max.IsZero() && value.After(c.Max) {
-			return value, fmt.Errorf("must be before %v", c.Max)
+			return value, fmt.Errorf("must be before %v", c.Max.Format(c.PresentationFormat()))
 		}
 		return value, nil
 	})
@@ -33,10 +58,10 @@ func (c *InputTime) Finalize(state *State) {
 
 func (c *InputTime) Process(*FormData) {
 	c.Binding.SetString(c.RawFormValue, func(s string) (time.Time, error) {
-		return time.ParseInLocation(time.DateOnly, s, c.Location)
+		return time.ParseInLocation(c.ValueFormat(), s, c.Location)
 	})
 }
 
 func (c *InputTime) FormattedValue() string {
-	return c.Binding.Value.In(c.Location).Format(time.DateOnly)
+	return c.Binding.Value.In(c.Location).Format(c.ValueFormat())
 }
